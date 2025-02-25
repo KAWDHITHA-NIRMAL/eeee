664c259219eccf8a162c6058df0e10ab2a7fd803
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2025-02-25 19:27:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBlI4LowrZO_zTgNZ4c5jI4O6ElxcxxXSc",
+  authDomain: "edu-lanka-9792d.firebaseapp.com",
+  projectId: "edu-lanka-9792d",
+  storageBucket: "edu-lanka-9792d.firebasestorage.app",
+  messagingSenderId: "769214395220",
+  appId: "1:769214395220:web:439e9cc46244ceb1148ec1",
+  measurementId: "G-3Y9H0VJLM7"
 };
 
 const app = initializeApp(firebaseConfig);
